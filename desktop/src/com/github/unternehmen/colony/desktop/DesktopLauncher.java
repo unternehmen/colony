843cb6bcdcaa8f39be74e50856d58d93c3fa22af
unternehmen <unternehmen@users.noreply.github.com>
2020-01-22 17:47:16 -0500

    Initial commit

---
+++ desktop/src/com/github/unternehmen/colony/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.unternehmen.colony.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.unternehmen.colony.ColonyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ColonyGame(), config);
+	}
+}
